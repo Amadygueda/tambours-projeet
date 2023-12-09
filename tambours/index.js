2a0b65c6a6b5b9499a3af8dcef878c7a1e2db2c2
Amadygueda <123624685+Amadygueda@users.noreply.github.com>
2023-12-09 12:15:51 -0500

    Update index.js

--- tambours/index.js
+++ tambours/index.js
@@ -4,7 +4,23 @@ for( var i=0;i<bouttons.length ; i++){
  bouttons[i].addEventListener("click",function cliker(){
    var lettre = this.innerHTML;
    appuyer (lettre);
+   dureAnimation(lettre)
  })    
+}
+/* detection clavier */
+
+document.addEventListener("keypress",function clavier (evens){
+    appuyer(evens.key)
+    dureAnimation(evens.key)
+})
+
+function dureAnimation(lettre){
+ var localisation = document.querySelector("."+lettre);
+ localisation.classList.add("pressed");
+ setTimeout(function(){
+    localisation.classList.remove("pressed"); 
+ },100);
+
 }
 function appuyer (bouttonAppuyer){
     switch(bouttonAppuyer){
