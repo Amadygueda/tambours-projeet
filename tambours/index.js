5433032f2d24d99fbeb39099a6b8bcc0e8b2aa24
Amadygueda <123624685+Amadygueda@users.noreply.github.com>
2023-12-09 12:14:30 -0500

    Add files via upload

---
+++ tambours/index.js
@@ -0,0 +1,41 @@
+/* detection des clicks*/
+var bouttons =document.getElementsByClassName("drum");
+for( var i=0;i<bouttons.length ; i++){
+ bouttons[i].addEventListener("click",function cliker(){
+   var lettre = this.innerHTML;
+   appuyer (lettre);
+ })    
+}
+function appuyer (bouttonAppuyer){
+    switch(bouttonAppuyer){
+        case "w":
+            var audios = new Audio("./sounds/tom-1.mp3");
+            audios.play();
+            break;
+            case "a":
+                var audios = new Audio("./sounds/tom-2.mp3");
+                audios.play();
+                break;
+                case "s":
+                    var audios = new Audio("./sounds/tom-3.mp3");
+                    audios.play();
+                    break; 
+                    case "d":
+                    var audios = new Audio("./sounds/tom-4.mp3");
+                    audios.play();
+                    break;    
+                    case "j":
+                        var audios = new Audio("./sounds/crash.mp3");
+                        audios.play();
+                        break;  
+                        case "k":
+                            var audios = new Audio("./sounds/kick-bass.mp3");
+                            audios.play();
+                            break;
+                            case "l":
+                            var audios = new Audio("./sounds/snare.mp3");
+                            audios.play();
+                            break;
+                                
+    }
+}
